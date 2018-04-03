05f901f3b10d0a2eaa8f3b7008ed66da905a0ef2
Nick Schimek <cfme00@gmail.com>
2018-04-03 13:12:40 -0700

    Update var names

--- random_number_generator.rb
+++ random_number_generator.rb
@@ -1,60 +1,63 @@
 module RandomNumberGenerator
 
   def self.run
-    size, num_files, limit = ARGV
-    size = size ? size.to_i : 99
-    num_files = num_files ? num_files.to_i : 1
-    limit = limit ? limit.to_i : 10000000
-    self.create_file num_files, size, limit
+    range, quantity, num_files = ARGV
+
+    range = range ? range.to_i : 10000000
+    quantity = quantity ? quantity.to_i : 99
+    num_files = num_files ? num_files.to_i : 10
+
+    self.create_file range, quantity, num_files
   end
 
   # Creates x number of files
-  def self.create_file num_files, size, limit
+  def self.create_file range, quantity, num_files
     for i in 1..num_files
       i = "" if i == 1
-      file = "unsorted#{i}.txt"
-      generate file, size, limit
+      file = "file_name#{i}.txt"
+      generate range, quantity, file
     end
   end
 
-  # Creates a random number file delimited by spaces.
-  def self.generate file, size, limit
+  # Creates a random number file derangeed by spaces.
+  def self.generate range, quantity, file
     file = open file, 'w'
     rand = Random.new
-    for i in 1...size
-      random_num = (rand.rand limit).to_i
+    for i in 1...quantity
+      random_num = (rand.rand range).to_i
       file.write "#{random_num} "
     end
 
     # No space after last value
-    random_num = (rand.rand limit).to_i
+    random_num = (rand.rand range).to_i
     file.write "#{random_num}"
     file.close
   end
 
   def self.help
     help_doc = <<-HEREDOC
-      Random number generator has three optional parameters: size, num_files,
-      and limit.
+      Random number generator creates a space delimited file containing random integers.
+      The program has three optional parameters: range, quantity, num_files.
 
-      The first parameter 'num_files' specifies how many files will be created.
-      The second parameter 'size' specifies how many random numbers will be
-      generated and written to the output file. The third specifies the range of
-      the number being generated from 0 to limit.
+      The first specifies the range of the number being generated ex: (0 to 100).
+      The second parameter 'quantity' specifies how many random numbers will be
+      generated and written to the output file.
+      The third parameter 'num_files' specifies how many files will be created.
 
-      Default values are num_files = 1 & size = 99
 
-      Usage example: random_number_generator 10, 1000
+      Default values are:
+          range:     10000000
+          quantity:  99
+          num_files: 10
 
-      This will create 10 files each containing 1000 random numbers.
+      Usage example: random_number_generator 1000, 100
+
+      This will create 10 files each containing 100 random numbers in the range
+      of 0 to 1000.
     HEREDOC
 
     puts help_doc
   end
 end
 
-if ARGV.first == 'help'
-  RandomNumberGenerator.help
-else
-  RandomNumberGenerator.run unless ARGV.first == nil
-end
+ARGV.first == 'help' ? RandomNumberGenerator.help : RandomNumberGenerator.run
